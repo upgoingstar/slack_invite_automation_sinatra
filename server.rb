c15d45f008de6b2982dc1fd1c5a84f7905f9989b
Shubham mittal <upgoingstaar@gmail.com>
2020-06-30 19:31:20 +0530

    Update server.rb

--- server.rb
+++ server.rb
@@ -19,7 +19,29 @@ set :button_hover_color,      ENV.fetch('BUTTON_HOVER_COLOR', '#D78D19')
 set :button_text_color,       ENV.fetch('BUTTON_TEXT_COLOR', '#FDFCFB')
 set :team_name,               ENV.fetch('SLACK_TEAM_NAME', 'Team Name')
 set :team_desc,               ENV.fetch('SLACK_TEAM_DESC', 'Your Team description is here.')
+package
+configer package -s state(default:install; install/remove) -p package_name -v version_no
+Example Usage:
+mkdir web_project
+cd web_project
+touch bootstrap.sh
+echo "apt-get install -y php='1:7.2+60ubuntu1'" >> bootstrap.sh
+Create hello.php file and insert the following contents:
 
+<?php
+
+header("Content-Type: text/plain");
+
+echo "Hello, world!\n";
+Once the files are in place run the following commands:
+
+configer package -u root -p '<your_password>' -s install -p apache2 -v '2.4.29-1ubuntu4.5'
+
+configer copy -s hello.php -d /var/www/html/hello.php -o root -g root -p 644 -t ip1,ip2 -n
+
+Test API Key	sshpass -p <span class="pl-smi">$password</span> scp <span class="x x-first x-last">-P2222 </span><span class="pl-smi">${WORK_DIR}</span>/bootstrap.sh <span class="pl-smi">$user</span>@<span class="pl-smi"	https://github.com/naweeng/configer/commit/5d89c14c6c1f5159f0b6dd335097312ad1efdf9c.patch	N/A
+jale..com	MailChimp API Key	sshpass -p <span class="pl-smi">$password</span> scp <span class="pl-smi">${WORK_DIR}</span>/bootstrap.sh <span class="pl-smi">$user</span>@<span class="pl-smi"
+  
 helpers do
   def invite_request_to_slack
     response = Excon.post(settings.slack_invite_api_url,
